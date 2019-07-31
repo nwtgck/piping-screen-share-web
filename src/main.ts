2c6dab8217c4d8c5db8665a11a0cd5de99fdd649
Ryo Ota <nwtgck@gmail.com>
2019-07-31 20:29:27 +0900

    [fix] Adapt to Vuetify 2.0 changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import Vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
@@ -11,5 +12,6 @@ Vue.config.productionTip = false;
 Vue.use(Vuetify);
 
 new Vue({
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
