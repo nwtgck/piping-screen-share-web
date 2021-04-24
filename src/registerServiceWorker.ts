e6360569808e4828cfce14856496bddb49900061
Ryo Ota <nwtgck@nwtgck.org>
2021-04-25 02:06:01 +0900

    disable Service Worker for now

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,7 +2,8 @@
 
 import { register } from 'register-service-worker';
 
-if (process.env.NODE_ENV === 'production') {
+// NOTE: Disable Service Worker for now
+if (false && process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
